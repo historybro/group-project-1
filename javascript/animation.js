e54cd66c743a0eb6dd1650ae3de58e32f8372241
CaitlynS <theprideignites@gmail.com>
2019-03-21 17:07:42 -0500

    Intro plays and pauses when opened/closed

--- javascript/animation.js
+++ javascript/animation.js
@@ -1,6 +1,19 @@
 $("#top-button-red").on("click", function(event) {
     console.log("#top-button-red pushed");
-    $( "#right, #top-right1, #top-right2, #border-screen, #screen img, #cross, #blue-button-left, #green-button-left, #orange-button-left, #square-button-left, #selectDisable, #blah" ).toggleClass( "closed" );
-    $( "#top-left1" ).toggleClass( "closed1" );
+    $( "#right, #top-right1, #top-right2, #border-screen, #screen img, #cross, #blue-button-left, #green-button-left, #orange-button-left, #square-button-left, #selectDisable, #blah").toggleClass( "closed" );
     $( "#blah" ).removeClass( "selectDisable" );
+    $( "#myVideo" ).toggleClass( "seeVideo" );
+    toggleMute();
 });
+
+function toggleMute() {
+
+    var video=document.getElementById("myVideo");
+  
+    if(video.muted){
+      video.muted = false;
+    } else {
+      video.muted = true;
+    }
+  
+  }
