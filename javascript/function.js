1ec9b64fbae4ea129ca3201112178ab6068aa810
Sean Massingill <25423602+historybro@users.noreply.github.com>
2019-03-30 11:13:01 -0500

    I know I said 3 pushes ago it was the last push...

--- javascript/function.js
+++ javascript/function.js
@@ -19,9 +19,7 @@ $(document).ready(function(){
 	  // Else use ‘day’ theme
       document.body.className = "day";
     }
-    setTimeout(function(){ $('#myModal').modal('show');}, 1000);
-    clearTimeout();
-    
+    $('#myModal').modal('show');
 });
 
 //initial variables
@@ -29,6 +27,7 @@ var pokemon;
 var idNum = 0;
 let gifname = [];
 var volumeon;
+
 
 //Better Time
 //clock function
@@ -50,6 +49,7 @@ function clock() {
     } else {
         document.getElementById('second').innerHTML= ":" + secs;
     };
+    
 }    
     
 setInterval(clock, 100);
